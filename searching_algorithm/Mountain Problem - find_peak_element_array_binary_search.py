a33df0906bcd8272b7114bdabcdff78b3252f29e
Dhruv Vatsa <67755889+Dhruvvatsa23@users.noreply.github.com>
2023-05-09 10:15:47 +0530

    Updated code
    Flow of the code:
    
    Note:  that this algorithm assumes that the input array is a valid mountain array, i.e., it has a peak element and the elements before the peak are in strictly increasing order, while the elements after the peak are in strictly decreasing order.
    
    1. First, the code reads in the input list lst from the user.
    
    2. Next, it initializes start to 0, end to the length of the list, and mid to the midpoint of the list using integer division.
    
    3. The find_peak function takes in the start, end, and mid indices and searches for the peak element recursively.
    
    4. Inside the find_peak function, there is a loop that iterates over all elements of the array until the peak element is found.
    
    5. The loop condition is not based on start and end indices as in the usual binary search, because we do not know where the peak element is located. Therefore, we iterate over all elements of the array until we find the peak element.
    
    6. Inside the loop, we compare the mid element with the next element in the list. If the mid element is less than the next element, we move start to mid+1, because the peak element must be on the right side of the current mid index.
    
    7. If the mid element is greater than or equal to the next element, we move end to mid, because the peak element must be on the left side of the current mid index.
    
    8. We then update the mid index as the new midpoint between start and end using integer division.
    
    9. When the loop ends, the start index points to the peak element, so we print it as the output.

--- searching_algorithm/Mountain Problem - find_peak_element_array_binary_search.py
+++ searching_algorithm/Mountain Problem - find_peak_element_array_binary_search.py
@@ -3,22 +3,18 @@
 lst = list(map(int, input().split()))
 
 start = 0
-end = len(lst)
-mid = (start + (end - start)//2)
+end = len(lst) - 1
 
-def find_peak(start, end, mid):
-    for i in range(len(lst)):
-      
-      if lst[mid] < lst[mid+1]:
+while start < end:
+    mid = (start + end) // 2
+
+    if lst[mid] < lst[mid + 1]:
         start = mid + 1
-      
-      else: 
+    
+    else:
         end = mid                       # this condition is different than the one used in regular binary search because "mid" can be at the peak element so
                                         # so if we use the condition i.e (end = mid - 1) we will be getting wrong answer as the mid will shift to the left-side
                                         # of the peak element
-      
-      mid = (start + (end - start)//2)
-    
-    print("Peak element is at index: ", start)
-    
-    find_peak(start, end, mid
+
+print("Peak element is at index:", start)
+
