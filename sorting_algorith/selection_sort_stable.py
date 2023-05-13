8870e43f8e4728b4b6ff894bd1f315ee2667d2f7
Dhruv Vatsa <67755889+Dhruvvatsa23@users.noreply.github.com>
2023-05-13 10:29:04 +0530

    Create selection_sort_stable.py
    Flow of the code:
    
    This is an implementation of a stable selection sort algorithm in Python. The function "stableSelectionSort" takes in an array "a" and its length "n" as input.
    
    1. The algorithm sorts the array in ascending order by first iterating over each element in the array. For each element, it then searches for the minimum value in the remaining unsorted portion of the array. If the minimum value is found, it is swapped with the current element.
    
    2. However, in order to maintain the stability of the sorting algorithm, the algorithm performs an additional check. If the minimum value to be swapped with the current element has the same value as another element that was already in the current position, the algorithm shifts all elements between those two positions one position to the right before inserting the minimum value at the current position.
    
    3. The "printArray" function simply prints the sorted array in ascending order.
    
    4. Finally, the driver code creates an array of integers, calls the "stableSelectionSort" function with the array and its length as input, and then prints the sorted array using the "printArray" function.

---
+++ sorting_algorith/selection_sort_stable.py
@@ -0,0 +1,21 @@
+def stableSelectionSort(array, n):
+    for i in range(n):
+        min_idx = i
+        for j in range(i + 1, n):
+            if array[min_idx] > array[j]:
+                min_idx = j
+        key = array[min_idx]
+        while min_idx > i:
+            array[min_idx] = array[min_idx - 1]
+            min_idx -= 1
+        array[i] = key
+ 
+def printArray(array, n):
+    for i in range(n):
+        print("%d" % array[i], end=" ")
+     
+# Driver Code
+array = [4, 5, 3, 2, 4, 1]
+n = len(array)
+stableSelectionSort(array, n)
+printArray(array, n)
